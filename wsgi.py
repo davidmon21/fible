7a1f280ec7f9724ab0f95c9dd2707fe82728923b
quietok(david) <quietok@protonmail.com>
2021-07-26 14:50:15 -0500

    attempt 3

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
-from app.main import app
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+bp = Blueprint('fible', __name__)
+
 if __name__ == "__main__":
-    app.run()
+    bp.run()
